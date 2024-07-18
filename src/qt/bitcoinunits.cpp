8607fcf1923238b7478b526a2c02450ea5c5947c
MerlinMagic2018 <antoinebrule77777@gmail.com>
2024-07-18 04:17:28 -0400

    corrected the ticker

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("ITC");
-    case mBTC: return QString("mITC");
-    case uBTC: return QString::fromUtf8("μITC");
+    case BTC: return QString("FRC");
+    case mBTC: return QString("mFRC");
+    case uBTC: return QString::fromUtf8("μFRC");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("ITC");
-    case mBTC: return QString("Milli-ITC (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-ITC (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("FRC");
+    case mBTC: return QString("Milli-FRC (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-FRC (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
